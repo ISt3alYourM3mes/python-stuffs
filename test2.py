4cc285fe60977b5e9700653b9c4cc9a23cc499bf
Oscar <80611319+oscar2558@users.noreply.github.com>
2021-03-13 23:36:15 -0500

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,6 @@
+equation1 = 6 + 3
+print("For the first thing I'll solve in a online code editor is 6 plus 3, and the answer is: " + str(equation1))
+equation2 = 4 + 9
+
+print("Equation2 is a lot more bigger than Equation1. We can prove this by having the result be true. ")
+print(str(equation1) > str(equation2))
